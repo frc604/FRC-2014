b5a2868e21fd9aa4f9c2c20a4c459b6c6f62d447
Michael Smith <mdsmtp@gmail.com>
2014-02-11 15:34:56 -0800

    Add in custom controller alongside normal joystick

--- src/com/_604robotics/robot2014/modes/TeleopMode.java
+++ src/com/_604robotics/robot2014/modes/TeleopMode.java
@@ -4,12 +4,15 @@ import com._604robotics.robotnik.coordinator.Coordinator;
 import com._604robotics.robotnik.coordinator.connectors.Binding;
 import com._604robotics.robotnik.coordinator.connectors.DataWire;
 import com._604robotics.robotnik.module.ModuleManager;
+import com._604robotics.robotnik.prefabs.controller.custom.CustomController;
 import com._604robotics.robotnik.prefabs.controller.joystick.JoystickController;
 import com._604robotics.robotnik.prefabs.trigger.TriggerOr;
 import com._604robotics.robotnik.prefabs.trigger.TriggerToggle;
 import com._604robotics.robotnik.trigger.TriggerAccess;
 
 public class TeleopMode extends Coordinator {
+    private final CustomController cust = new CustomController("Left");
+    
     private final JoystickController leftDrive = new JoystickController(1);
     private final JoystickController rightDrive = new JoystickController(2);
     private final JoystickController manipulator = new JoystickController(3);
@@ -46,12 +49,15 @@ public class TeleopMode extends Coordinator {
                 this.bind(new Binding(modules.getModule("Drive").getAction("Stick Drive"), modules.getModule("Dashboard").getTrigger("Stick Drive")));
                 this.fill(new DataWire(modules.getModule("Drive").getAction("Stick Drive"), "throttle", leftDrive.axisY));
                 this.fill(new DataWire(modules.getModule("Drive").getAction("Stick Drive"), "turn", leftDrive.axisX));
+                
+                //this.fill(new DataWire(modules.getModule("Drive").getAction("Stick Drive"), "throttle", cust.getAxis("Y")));
+                //this.fill(new DataWire(modules.getModule("Drive").getAction("Stick Drive"), "turn", cust.getAxis("X")));
             }
             
             /* Shifter */
             {
                 final TriggerToggle toggle = new TriggerToggle(new TriggerOr(new TriggerAccess[] {
-                    leftDrive.buttons.Button1, rightDrive.buttons.Button1
+                    leftDrive.buttons.Button1, rightDrive.buttons.Button1, cust.getButton("Trigger")
                 }), false);
                 this.bind(new Binding(modules.getModule("Shifter").getAction("Low Gear"), toggle.off));
                 this.bind(new Binding(modules.getModule("Shifter").getAction("High Gear"), toggle.on));
@@ -65,14 +71,17 @@ public class TeleopMode extends Coordinator {
                 this.bind(new Binding(modules.getModule("Shooter").getAction("Deploy"), manipulator.buttons.Button1));
             }
             
+            /* Rotation */
+            {
+                this.bind(new Binding(modules.getModule("Rotation").getAction("Manual"), manipulator.buttons.Button8));
+                this.fill(new DataWire(modules.getModule("Rotation").getAction("Manual"), "power", manipulator.axisY));
+                
+                this.bind(new Binding(modules.getModule("Rotation").getAction("Manual Angle"), manipulator.buttons.Button9));
+                this.fill(new DataWire(modules.getModule("Rotation").getAction("Manual Angle"), "setpoint", modules.getModule("Dashboard").getData("Manual Rotation Angle")));
+            }
+            
             /* Flower */
             {
-                this.bind(new Binding(modules.getModule("Flower").getAction("Manual"), manipulator.buttons.Button8));
-                this.fill(new DataWire(modules.getModule("Flower").getAction("Manual"), "power", manipulator.axisY));
-                
-                this.bind(new Binding(modules.getModule("Flower").getAction("Manual Angle"), manipulator.buttons.Button9));
-                this.fill(new DataWire(modules.getModule("Flower").getAction("Manual Angle"), "setpoint", modules.getModule("Dashboard").getData("Manual Rotation Angle")));
-                
                 this.bind(new Binding(modules.getModule("Flower").getAction("Stow"), manipulator.buttons.Button3));
                 this.bind(new Binding(modules.getModule("Flower").getAction("Catch"), manipulator.buttons.Button2));
                 this.bind(new Binding(modules.getModule("Flower").getAction("Shoot"), manipulator.buttons.Button5));
